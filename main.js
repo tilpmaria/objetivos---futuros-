4781b77b1e241add60362a84776d903db162781e
tilpmaria <tilp.maria@escola.pr.gov.br>
2026-05-14 08:33:23 -0300

    .

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2026-12-10T00:00:00");
 const tempoObjetivo2 = new Date("2026-12-18T00:00:00");
 const tempoObjetivo3 = new Date("2026-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2026-01-01T00:00:00");
+const tempoObjetivo4 = new Date("2027-01-01T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
